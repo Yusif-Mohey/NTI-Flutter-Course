55cadac8c88e195bf43df3f98e9999dc5391b2c1
moheyy28@gamil.com <moheyy28@gmail.com>
2026-02-28 02:26:22 +0200

    tesk 1

---
+++ bin/task_1.dart
@@ -0,0 +1,15 @@
+import 'package:task_1/task_1.dart' as task_1;
+
+void main(List<String> arguments) {
+  print('Task 1 : ${task_1.authProfile('student', '123', true)}');
+  print('Task 2 : ${task_1.discountPrice(100, true, false)}');
+  print('Task 3 : ${task_1.checkExamResult(60, 80)}');
+  print('Task 4 : ${task_1.checkLoanApproval(6000, 25, false)}');
+  print('Task 5 : ${task_1.calculateTotalAmount(250, 10)}');
+  print('Task 6 : ${task_1.checkBonus(4, 5)}');
+  print('Task 7 : ${task_1.checkDoorAccess(true, true)}');
+  print('Task 8 : ${task_1.getConsumptionCategory(300)}');
+  print('Task 9 : ${task_1.checkLevelUnlock(12, 75)}');
+  print('Task 10 : ${task_1.checkRideStart(true, 2, 4)}');
+  //
+}
